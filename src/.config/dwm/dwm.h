3f8f03aa1638dec48c7f41ee11170f5e9e7dbb2b
mzeinali <mzeinali@tutanota.com>
2021-01-18 00:04:10 +0330

    Add mount and unmount shrtcut for dwm

--- src/.config/dwm/dwm.h
+++ src/.config/dwm/dwm.h
@@ -236,6 +236,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
 	{ MODKEY,			XK_F4,		spawn,		SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
+	{ MODKEY,			XK_F9,		spawn,		SHCMD("menumount") },
+	{ MODKEY,			XK_F10,		spawn,		SHCMD("menuumount") },
 	{ MODKEY,			XK_F11,		spawn,		SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --profile=low-latency --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1) --vf=vflip") },
 	{ MODKEY,			XK_F12,		spawn,		SHCMD("remaps & notify-send \\\"⌨️ Keyboard remapping...\\\" \\\"Re-running keyboard defaults for any newly plugged-in keyboards.\\\"") },
 	{ MODKEY,			XK_space,	zoom,		{0} },
