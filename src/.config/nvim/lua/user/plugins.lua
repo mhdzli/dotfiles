0d8a8736dcc545b3badf8ff58a345daac410843b
mzeinali <m@zmim.ir>
2022-08-06 05:00:28 +0430

    vimwiki configs

--- src/.config/nvim/lua/user/plugins.lua
+++ src/.config/nvim/lua/user/plugins.lua
@@ -97,7 +97,25 @@ return packer.startup(function(use)
         "nvim-treesitter/playground",
     }
 
-    use "vimwiki/vimwiki"
+    use {
+        "vimwiki/vimwiki",
+        config = function()
+            vim.g.vimwiki_list = {
+                {
+                    path = '~/.local/share/disroot/Notes',
+                    syntax = 'markdown',
+                    ext = '.md',
+                }
+            }
+            vim.g.vimwiki_ext2syntax = {
+                ['.md'] = 'markdown',
+                ['.markdown'] = 'markdown',
+                ['.mdown'] = 'markdown',
+                ['.Rmd'] = 'markdown',
+                ['.rmd'] = 'markdown',
+            }
+        end
+    }
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
