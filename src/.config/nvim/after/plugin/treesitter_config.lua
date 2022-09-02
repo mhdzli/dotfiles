919120488597c5fb6af29b9f6e41278b92742f4d
mzeinali <m@zmim.ir>
2022-09-02 04:43:34 +0430

    Add treesitter_context

--- src/.config/nvim/after/plugin/treesitter_config.lua
+++ src/.config/nvim/after/plugin/treesitter_config.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "c", "lua", "python", "vim", "regex", "markdown", "html", "json", "scss", "css", "typescript", "make", "bash" },
+  ensure_installed = { "c", "lua", "python", "vim", "regex", "markdown", "html", "json", "scss", "css", "typescript", "make", "bash", "yaml", "toml" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- src/.config/nvim/lua/user/plugins.lua
+++ src/.config/nvim/lua/user/plugins.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootstrap
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
@@ -99,6 +100,7 @@ return packer.startup(function(use)
       run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
     },
     "nvim-treesitter/playground",
+    "nvim-treesitter/nvim-treesitter-context",
   }
 
   use {
